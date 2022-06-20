f4648cc4bcf80b940a7a3a821cff2f5783f3fa6c
kadun1 <kadunjj@gmail.com>
2022-06-21 00:04:29 +0900

    안드로이드 실습 키즈 드로잉 앱 복습

--- app/src/main/java/com/tutorials/kidsdwawingapp/DrawingView.kt
+++ app/src/main/java/com/tutorials/kidsdwawingapp/DrawingView.kt
@@ -23,6 +23,7 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
         setUpDrawing()
     }
 
+    // Redo 기능도 구현해보면 좋음
     fun onClickUndo() {
         if (mPaths.size > 0) {
             mUndoPaths.add(mPaths.removeAt(mPaths.size - 1))
